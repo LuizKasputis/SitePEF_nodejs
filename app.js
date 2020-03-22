b28994f14a634fe9640f08875536a1a41bd18382
Luiz Guilherme <luiz.kasputis@hotmail.com>
2020-03-22 19:37:42 -0300

    adicionando BD

--- app.js
+++ app.js
@@ -5,11 +5,44 @@ require('marko/express');
 var express = require('express');
 var app = express();
 app.use(express.static('./'));
+var mysql = require('mysql');
+var resultado;
+
+
+
+
+app.get('/notas', function(req, resp){
+
+	var con = mysql.createConnection({
+	  host: "mysql669.umbler.com",
+	  port: "41890",
+	  user: "lulizao",
+	  password: "zaninill1",
+	  database: "notas_alunos"
+	});
+
+
+
+	var db = con.connect(function(err) {
+		  if (err) throw err;
+		  var db = con.query("SELECT * FROM aluno", function (err, result, fields) {
+		    if (err) throw err;
+		    resultado = result;
+		  });
+	});
+
+
+    resp.marko(
+    			require('./notas.marko'),{
+    				alunos : resultado
+    			});
+});
+
 
 
 app.get('/', function(req, resp){
     resp.marko(
-    				require('./modeloInterativo.marko'),
+    				require('./modeloInterativo.marko')
     			   );
 });
                                 
